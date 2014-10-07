922d1e1c0de17850c01ff9d42e5bd892f6731c5d
Gabriel Farrell <g@grrawr.com>
2014-10-07 04:25:43 -0400

    Just return wrapper on empty html

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var h = require('virtual-hyperscript')
 
 
 module.exports = function (selector, html) {
-  var wrapper
+  var wrapper = h(selector)
+  if (!html) return wrapper
   if (typeof window == 'undefined') return h(selector, html)
   else {
-    wrapper = h(selector)
     p = wrapper.properties
     return fromHTML('<' + wrapper.tagName + (p.id ? ' id="' + p.id + '"' : '') +
       (p.className ? ' class="' + p.className + '"' : '') + '>' + html + '</' + 
